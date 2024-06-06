a2868faffbfb71ef298006bc6aefbfd63dbee269
Naomi Lizano <naomilizano@gmail.com>
2024-06-06 00:09:22 -0500

    Changes by camig

---
+++ src/main/java/com/proyecto/plataforma/repository/AdminRepository.java
@@ -0,0 +1,8 @@
+package com.proyecto.plataforma.repository;
+
+import com.proyecto.plataforma.data.Admin;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface AdminRepository extends MongoRepository<Admin, String> {
+    Admin findByCorreo(String correo);
+}
